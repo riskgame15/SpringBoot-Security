8440b6d77b36b48f73f9757a147a185c4944c27d
riskgame15 <tttungcr@gmail.com>
2025-02-06 09:02:05 +0700

    edit config

--- src/main/java/com/springbootsecurity/config/SecurityConfig.java
+++ src/main/java/com/springbootsecurity/config/SecurityConfig.java
@@ -1,14 +1,20 @@
 package com.springbootsecurity.config;
 
 
+import com.springbootsecurity.service.user.IAppUserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
@@ -20,17 +26,31 @@ public class SecurityConfig {
     // xac thuc
     @Bean
     public PasswordEncoder passwordEncoder() {
-//        return new BCryptPasswordEncoder();
-        return NoOpPasswordEncoder.getInstance();
+        return new BCryptPasswordEncoder(10);
+//        return NoOpPasswordEncoder.getInstance();
     }
 
+    @Autowired
+    private IAppUserService userService;
+    //    xac thuc
     @Bean
-    public InMemoryUserDetailsManager inMemoryUserDetailsManager() {
-        // build pattern
-      UserDetails user = User.withUsername("admin").password(passwordEncoder().encode("123456")).roles("USER").build();
-      UserDetails admin = User.withUsername("saobekhonglac").password(passwordEncoder().encode("123456")).roles("ADMIN").build();
-      return new InMemoryUserDetailsManager(user, admin);
+    public AuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+//        service phai la 1 instance cua UserDetailService
+        authProvider.setUserDetailsService((UserDetailsService) userService);
+//        authProvider.setPasswordEncoder(NoOpPasswordEncoder.getInstance());
+        authProvider.setPasswordEncoder(passwordEncoder());
+        return authProvider;
     }
+
+//    @Bean
+//    public InMemoryUserDetailsManager inMemoryUserDetailsManager() {
+//        // build pattern
+//        //        chuyen tu user trong db -  userdetail
+//      UserDetails user = User.withUsername("admin").password("$2a$10$AhRVCCzzASq6tMtLrm8WyuncN4YVOEeEmeoxGRskQxBgdsdXtWEiS").roles("USER").build();
+//      UserDetails admin = User.withUsername("saobekhonglac").password(passwordEncoder().encode("123456")).roles("ADMIN").build();
+//      return new InMemoryUserDetailsManager(user, admin);
+//    }
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
